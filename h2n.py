e3b4cdb86f0c969b9754256e5696cd072daf6ed5
ksang <kaitoy@qq.com>
2024-04-01 10:03:59 +0800

    refactor

--- h2n.py
+++ h2n.py
@@ -1,51 +1,57 @@
 from torch import nn
 import torch
 from torchvision.transforms import GaussianBlur
-
-class Sobel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.filter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=1, bias=False)
-
-        Gx = torch.tensor([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
-        Gy = torch.tensor([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
-        G = torch.cat([Gx.unsqueeze(0), Gy.unsqueeze(0)], 0)
-        G = G.unsqueeze(1)
-        self.filter.weight = nn.Parameter(G, requires_grad=False)
-
-    def forward(self, x):
-        x = self.filter(x)
-        return x
     
 class HeightToNormal(nn.Module):
     def __init__(
             self,
-            filter = None,
-            transforms = GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 5)),
+            filter_type = "sobel",
+            blur = GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 5)),
             strength: float = 1.0,
             invert_x: bool = False,
             invert_y: bool = False,
             invert_height: bool = False,
         ):
         super().__init__()
-        self.filter = filter
+        self.filter_kernel = self.get_filter_kernel(filter_type)
         self.strength = strength
         self.invert_x = invert_x
         self.invert_y = invert_y
         self.invert_height = invert_height
-        self.transforms = transforms
+        self.blur = blur
 
+    def get_filter_kernel(self, filter_type): # 2x1x3x3  
+        if filter_type == "sobel":
+            sobel_x = torch.tensor([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+            sobel_y = torch.tensor([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+            return torch.stack([sobel_x, sobel_y]).unsqueeze(1)   
+        elif filter_type == "scharr":
+            scharr_x = torch.tensor([[47, 0, -47], [162, 0, -162], [47, 0.0, -47]]) / 255.0
+            scharr_y = torch.tensor([[47, 162, 47], [0, 0, 0], [-47, -162, -47]]) / 255.0
+            return torch.stack([scharr_x, scharr_y]).unsqueeze(1) 
+        else:
+            raise ValueError(f"Filter type {filter_type} not supported.")   
+        
     def pre_process(self, height):
         height = -1.0 * height + 1.0 if not self.invert_height else height
-        if self.transforms:
-            height = self.transforms(height)
+        if self.blur:
+            height = self.blur(height)
         return height
 
+    def filter(self, x):
+        x = self.pre_process(x)
+        x = torch.nn.functional.conv2d(x, self.filter_kernel, bias=None, stride=1, padding=1, groups=1)   # Bx2xHxW
+        return x
+    
+    def filter_grad(self, x):
+        x = self.filter(x)
+        Gx, Gy = x[:,0], x[:,1]
+        return torch.sqrt(Gx**2 + Gy**2)
+    
     def forward(self, height):
-        x = self.pre_process(height)
-        out = self.filter(x)   # height Bx1xHxW, out: Bx2xHxW
-        dx = -1.0 * out[:, 0] if self.invert_x else out[:, 0]
-        dy = -1.0 * out[:, 1] if self.invert_y else out[:, 1]
-        normal = torch.stack([dx, dy, torch.ones(dx.shape)/self.strength], dim=1)
+        x = self.filter(height)
+        dx = -1.0 * x[:, 0] if self.invert_x else x[:, 0]
+        dy = -1.0 * x[:, 1] if self.invert_y else x[:, 1]
+        normal = torch.stack([dx, dy, torch.ones(dx.shape, device=dx.device)/self.strength], dim=1)
         normal = torch.nn.functional.normalize(normal, dim=1)
         return normal
